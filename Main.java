73653d65d65c15356b5feb0ddfd0a26b35d9bbce
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2022-11-08 12:46:08 +0900

    ñlªvZł«é悤ɏC³

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
   public class Main{  
   public static void main(String[] args) {
   Scanner scanner = new Scanner(System.in);
+  for(int i = 0; i<2;i++){
   System.out.print("身長(cm)=");
   double height = scanner.nextDouble() / 100;
   System.out.print("体重(kg)=");
@@ -13,5 +14,6 @@ import java.util.Scanner;
   String sv = String.format("%.2f", v1);
   double v2 = Double.parseDouble(sv);
   System.out.println("BMIを小数点第二位に表すと="+v2);
+  }
   }
 }
