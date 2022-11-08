2d09ed8b9a9c6055416d5ebce59f57e941e29db8
takahashi <ryo.takahashi.sys22@morijyobi.ac.jp>
2022-11-08 11:09:03 +0900

    作成しました

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+  public class Main{  
+  public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
+  System.out.print("身長(cm)=");
+  double height = scanner.nextDouble() / 100;
+  System.out.print("体重(kg)=");
+  double weight = scanner.nextDouble();
+  System.out.println("BMI=" + weight / height / height);
+
+  double v1 = weight / height / height;
+  String sv = String.format("%.2f", v1);
+  double v2 = Double.parseDouble(sv);
+  System.out.println("BMIを小数点第二位に表すと="+v2);
+  }
+}
